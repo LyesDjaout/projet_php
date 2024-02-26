f723b80a4f7108595f12b9499b8edb89d0538454
djaout lyes <djaoutlyes24@gmail.com>
2024-02-27 00:16:25 +0100

    add controllers

---
+++ app/controllers/display_autor.php
@@ -0,0 +1,12 @@
+<?php
+
+function displayAuthor(string $authorEmail, array $users): string
+{
+    foreach ($users as $user) {
+        if ($authorEmail === $user['email']) {
+            return $user['full_name'] . '(' . $user['age'] . ' ans)';
+        }
+    }
+
+    return 'Auteur inconnu';
+}
---
+++ app/controllers/register.php
@@ -0,0 +1,5 @@
+<?php
+
+function register(){
+    require('app/templates/register.php');
+}

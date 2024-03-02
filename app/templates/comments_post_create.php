916793339a9dbc76472d386e9562acb8e5110240
djaout lyes <djaoutlyes24@gmail.com>
2024-03-03 00:02:00 +0100

    add style to comments pst create page

--- app/templates/comments_post_create.php
+++ app/templates/comments_post_create.php
@@ -1,9 +1,13 @@
 
 <?php $title = "Création de commentaire"; ?>
 <?php ob_start(); ?>
-    <h1>Commentaire ajouté avec succès !</h1>
-    <p><b>Note</b> : <?php echo htmlspecialchars($review); ?> / 5</p>
-    <p><b>Votre commentaire</b> : <?php echo htmlspecialchars(strip_tags($comment)); ?></p>
+
+    <section class="flex-container section-container">
+        <h1>Commentaire ajouté avec succès !</h1>
+        <p><b>Note</b> : <?php echo htmlspecialchars($review); ?> / 5</p>
+        <p><b>Votre commentaire</b> : <?php echo htmlspecialchars(strip_tags($comment)); ?></p>
+    </section>
+
 <?php $content = ob_get_clean(); ?>
 <?php require('layout.php') ?>
 
